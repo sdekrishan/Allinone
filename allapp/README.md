176ecf3aa86a9e7be9078ce94e17dee33417cfff
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2023-02-27 10:21:36 +0530

    data stored

--- allapp/README.md
+++ allapp/README.md
@@ -1,3 +1,8 @@
 # Description
 
 ### Allone app consists many javascript as well as react projects
+
+1. Scroll and get the data 
+scroll and get the data.png
+
+2.
--- allapp/src/App.js
+++ allapp/src/App.js
@@ -1,6 +1,22 @@
 import './App.css';
 
 function App() {
+  const data = [
+    {
+    link:"https://polite-lolly-3387d8.netlify.app/",
+    img:"",
+    github:"",
+    name:"Scroll and Get",
+    type:"javascript"
+    },
+    {
+      link:"https://polite-lolly-3387d8.netlify.app/",
+      img:"",
+      github:"",
+      name:"Players and Games",
+      type:"javascript"
+      },
+  ]
   return (
     <div className="App">
      
