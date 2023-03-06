7df1cb8b5e5f5ec7c08565cb12e415d0a73955cd
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2023-03-06 14:22:50 +0530

    some changes

--- allapp/src/App.js
+++ allapp/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   const data = [
     {
-    link:"https://polite-lolly-3387d8.netlify.app/",
+    link:"https://singular-treacle-950e2b.netlify.app/",
     img:"",
     github:"",
     name:"Scroll and Get",
@@ -16,10 +16,29 @@ function App() {
       name:"Players and Games",
       type:"javascript"
       },
+      {
+        link:"https://storied-meerkat-f3c144.netlify.app/",
+      img:"",
+      github:"",
+      name:"Web Coffee Shop",
+      type:"javascript"
+      },
+      {
+        link:"https://storied-meerkat-f3c144.netlify.app/",
+        img:"",
+        github:"",
+        name:"Web Coffee Shop",
+        type:"javascript"
+      }
   ]
   return (
     <div className="App">
-     
+     {data && data.map((el,ind)=>{
+      return <div>
+        
+
+      </div>
+     })}
     </div>
   );
 }
