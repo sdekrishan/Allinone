ef54f1e53221ae85afc79e635c17f6624923eede
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2023-03-13 12:57:41 +0530

    kstocks app added

--- allapp/src/App.js
+++ allapp/src/App.js
@@ -1,45 +1,106 @@
+import { Box, flexbox, Image, Text } from '@chakra-ui/react';
 import './App.css';
-
+import {images} from './Container/index'
 function App() {
   const data = [
     {
-    link:"https://singular-treacle-950e2b.netlify.app/",
-    img:"",
+    link:"https://singular-treacle-950e2b.netlify.app/",//1
+    img:images.mock1,
     github:"",
     name:"Scroll and Get",
     type:"javascript"
     },
     {
-      link:"https://polite-lolly-3387d8.netlify.app/",
-      img:"",
+      link:"https://polite-lolly-3387d8.netlify.app/",//2
+      img:images.mock2,
       github:"",
       name:"Players and Games",
       type:"javascript"
       },
       {
-        link:"https://storied-meerkat-f3c144.netlify.app/",
-      img:"",
+        link:"https://storied-meerkat-f3c144.netlify.app/",//3
+      img:images.mock3,
       github:"",
       name:"Web Coffee Shop",
       type:"javascript"
       },
       {
-        link:"https://storied-meerkat-f3c144.netlify.app/",
-        img:"",
+        link:"https://storied-meerkat-f3c144.netlify.app/",//4
+        img:images.weather,
         github:"",
-        name:"Web Coffee Shop",
+        name:"Weather Forecast",
         type:"javascript"
-      }
+      },
+      {
+        link:"https://tic-tac-toe-wknv.vercel.app/",//5
+        img:images.tictac,
+        github:"",
+        name:"Tic Tac Toe",
+        type:"javascript"
+      },
+      {
+        link:"https://snake-game-fnql.vercel.app/",//6
+        img:images.snake,
+        github:"",
+        name:"Snake Game",
+        type:"javascript"
+      },
+      {
+        link:"https://unrivaled-meerkat-06c2bd.netlify.app/",//7
+        img:images.library,
+        github:"",
+        name:"Our Library ",
+        type:"javascript"
+        },
+        {
+          link:"https://mock-eight.vercel.app/",//8
+          img:images.pokemon,
+          github:"",
+          name:"PokeHub",
+          type:"React"
+          },
+          {
+            link:"https://storied-meerkat-f3c144.netlify.app/",//9
+          img:images.mock7,
+          github:"",
+          name:"KStocks",
+          type:"javascript"
+          },
+          {
+            link:"https://storied-meerkat-f3c144.netlify.app/",//10
+            img:"",
+            github:"",
+            name:"Weather Forecast",
+            type:"javascript"
+          },
+          {
+            link:"https://storied-meerkat-f3c144.netlify.app/",//11
+            img:"",
+            github:"",
+            name:"Tic Tac Toe",
+            type:"javascript"
+          },
+          {
+            link:"https://storied-meerkat-f3c144.netlify.app/",//12
+            img:"",
+            github:"",
+            name:"Snake Game",
+            type:"javascript"
+          },
+
+          
+
+      
   ]
   return (
-    <div className="App">
+    <Box className="App" display={'flex'} flexWrap='wrap' border='1px solid black'>
      {data && data.map((el,ind)=>{
-      return <div>
-        
-
-      </div>
+      return <Box key={ind} w='25%' border='1px solid black' >
+        <Image src={el.img} />
+        <Text> {el.name}</Text>
+      </Box>
      })}
-    </div>
+    </Box>
   );
 }
 
---
+++ allapp/src/Container/image.js
@@ -0,0 +1,13 @@
+import mock1 from '../assets/mock1.png';
+import mock2 from '../assets/mock2.png'
+import mock3 from '../assets/mock3.png';
+import mock4 from '../assets/mock4.png';
+import pokemon from '../assets/pokemon.png';
+import weather from '../assets/weather.png';
+import social from '../assets/social.png';
+import snake from '../assets/snake.png';
+import tictac from '../assets/tictac.png';
+import library from '../assets/library.png';
+import mock7 from '../assets/mock7.png'
+
+export default {mock1,mock2,mock3,mock4,pokemon,weather,social,snake,tictac,library,mock7}
